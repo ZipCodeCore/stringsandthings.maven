9d73887eac90bfb323e27a0367bfc5e3454f904c
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-06-28 19:36:33 -0400

    modified ContainsEqualNumberOfIsAndNot to successfully pass tests

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,7 @@
 package io.zipcoder;
 
 
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,55 +33,59 @@ public class StringsAndThings {
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
+     * removeString("Hello there", "e") //  Should return "Hllo thr"
+     * removeString("Hello there", "x") // Should return "Hello there"
      */
-    public String removeString(String base, String remove){
+    public String removeString(String base, String remove) {
         String res = base.replaceAll("(?i)" + remove, "");
+        System.out.println(res);
         return res;
     }
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+     * containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+     * containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        String find01 = "is";
-        String find02 = "not";
-        int lastIndex01 = 0;
-        int lastIndex02 = 0;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        ArrayList<String> isArrList = new ArrayList<>();
+        ArrayList<String> notArrList = new ArrayList<>();
+        boolean isEqual = false;
+        String is = "is";
+        String not = "not";
 
-        ArrayList<String> arr = new ArrayList<>();
-
-        int count01 = 0;
-        int count02 = 0;
-        boolean equalCount = false;
-
-        while (input.contains(find02) && input.contains(find01)) {
-            input = input.replaceFirst(find02, "");
-            input = input.replaceFirst(find01, "");
-            count01++;
-            count02++;
+        for (int i=0; i<=input.length()-1; i++) {
+            if (input.regionMatches(true,i,is, 0, is.length())) {
+//                System.out.println("found is");
+                isArrList.add("added");
+            }
+            if (input.regionMatches(true, i, not, 0, not.length())) {
+//                System.out.println("found not");
+                notArrList.add("added");
+            }
         }
-        System.out.println(count01);
-        System.out.println(count02);
-        return equalCount;
+//        System.out.println("length of 'is' list= " + isArrList.size());
+//        System.out.println("length of 'not' list= " + notArrList.size());
+        if (isArrList.size() == notArrList.size()) {
+            isEqual = true;
+        }
+        return isEqual;
     }
+
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
      * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
+     * gHappy("xxgxx") // Should return  false
+     * gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
+    public Boolean gIsHappy(String input) {
         return null;
     }
 
@@ -89,10 +94,10 @@ public class StringsAndThings {
      * We'll say that a "triple" in a string is a char appearing three times in a row.
      * Return the number of triples in the given string. The triples may overlap.
      * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
+     * countTriple("xxxabyyyycd") // Should return 3
+     * countTriple("a") // Should return 0
      */
-    public Integer countTriple(String input){
+    public Integer countTriple(String input) {
         return null;
     }
 }
